602a5721fca4e416a72dfa961c7c2252edcef1d4
Bryan <bryan_stevens314@yahoo.com>
2019-01-29 18:56:09 -0800

    Initial Commit

---
+++ Core_Bluetooth/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Core_Bluetooth
+//
+//  Created by Bryan Stevens on 1/29/19.
+//  Copyright © 2019 IGMOTIVE. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Core_BluetoothTests/Core_BluetoothTests.swift
@@ -0,0 +1,34 @@
+//
+//  Core_BluetoothTests.swift
+//  Core_BluetoothTests
+//
+//  Created by Bryan Stevens on 1/29/19.
+//  Copyright © 2019 IGMOTIVE. All rights reserved.
+//
+
+import XCTest
+@testable import Core_Bluetooth
+
+class Core_BluetoothTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
